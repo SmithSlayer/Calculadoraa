f437f81ba43e2d4f9632b8d48c9f14aab72d5bb5
jeffersonkauhan42 <jeffersonkauhan42@gmail.com>
2025-02-01 13:52:46 +0000

    atualização

--- lib/calculadora.dart
+++ lib/calculadora.dart
@@ -1,6 +1,8 @@
 import 'package:expressions/expressions.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
 
+// Widget da Calculadora
 class Calculadora extends StatefulWidget {
   const Calculadora({super.key});
 
@@ -9,45 +11,95 @@ class Calculadora extends StatefulWidget {
 }
 
 class _CalculadoraState extends State<Calculadora> {
-  final String _limpar = 'Limpar';
   String _expressao = '';
   String _resultado = '';
+  bool _isDestroyed = false;
 
+  // Função que trata a lógica dos botões
   void _pressionarBotao(String valor) {
     setState(() {
-      if (valor == _limpar) {
-        _expressao = '';
-        _resultado = '';
-      } else if (valor == '=') {
+      if (_isDestroyed) return;
+
+      if (valor == "=") {
         _calcularResultado();
+      } else if (valor == '√') {
+        _calcularRaiz();
+      } else if (valor == '%') {
+        _calcularPorcentagem();
+      } else if (valor == 'C') {
+        _apagarTudo();
       } else {
         _expressao += valor;
       }
     });
   }
 
+  // Função para apagar tudo
+  void _apagarTudo() {
+    _expressao = '';
+    _resultado = '';
+  }
+
+  // Função para calcular o resultado da expressão
   void _calcularResultado() {
     try {
       _resultado = _avaliarExpressao(_expressao).toString();
+
+      if (_resultado == '17') {
+        _ativarAutoDestruicao();
+      }
     } catch (e) {
-      _resultado = 'Não é possivel calcular';
+      _resultado = "Erro";
     }
   }
 
+  // Função para calcular a raiz quadrada
+  void _calcularRaiz() {
+    try {
+      double numero = double.parse(_expressao);
+      _resultado = (numero >= 0 ? sqrt(numero).toString() : 'Erro');
+    } catch (e) {
+      _resultado = 'Erro';
+    }
+  }
+
+  // Função para calcular a porcentagem
+  void _calcularPorcentagem() {
+    try {
+      double numero = double.parse(_expressao);
+      _resultado = (numero / 100).toString();
+    } catch (e) {
+      _resultado = 'Erro';
+    }
+  }
+
+  // Função para avaliar a expressão usando a biblioteca expressions
   double _avaliarExpressao(String expressao) {
     expressao = expressao.replaceAll('x', '*');
     expressao = expressao.replaceAll('÷', '/');
-    // Avaliar a espressao com a biblioteca expressions
     ExpressionEvaluator avaliador = const ExpressionEvaluator();
     double resultado = avaliador.eval(Expression.parse(expressao), {});
     return resultado;
   }
 
+  // Função para ativar a "auto destruição" (exemplo fictício)
+  void _ativarAutoDestruicao() async {
+    setState(() {
+      _resultado = 'Ativando auto destruição...';
+    });
+
+    await Future.delayed(Duration(seconds: 2));
+    setState(() {
+      _isDestroyed = true;
+    });
+  }
+
+  // Função que cria um botão da calculadora
   Widget _botao(String valor) {
     return TextButton(
       child: Text(
         valor,
-        style: const TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+        style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
       ),
       onPressed: () => _pressionarBotao(valor),
     );
@@ -57,46 +109,51 @@ class _CalculadoraState extends State<Calculadora> {
   Widget build(BuildContext context) {
     return Column(
       children: [
+        // Exibe a expressão
         Expanded(
           child: Text(
             _expressao,
             style: const TextStyle(fontSize: 24),
           ),
         ),
+        // Exibe o resultado
         Expanded(
           child: Text(
             _resultado,
             style: const TextStyle(fontSize: 24),
           ),
         ),
-        Expanded(
-          flex: 3,
-          child: GridView.count(
-            crossAxisCount: 4,
-            childAspectRatio: 2,
-            children: [
-              _botao('7'),
-              _botao('8'),
-              _botao('9'),
-              _botao('÷'),
-              _botao('4'),
-              _botao('5'),
-              _botao('6'),
-              _botao('x'),
-              _botao('1'),
-              _botao('2'),
-              _botao('3'),
-              _botao('-'),
-              _botao('0'),
-              _botao('.'),
-              _botao('='),
-              _botao('+'),
-            ],
+        // Exibe os botões da calculadora
+        if (!_isDestroyed)
+          Expanded(
+            flex: 3,
+            child: GridView.count(
+              crossAxisCount: 4,
+              childAspectRatio: 2,
+              children: [
+                _botao("C"),
+                _botao(""),
+                _botao("%"),
+                _botao("√"),
+                _botao("7"),
+                _botao("8"),
+                _botao("9"),
+                _botao("÷"),
+                _botao("4"),
+                _botao("5"),
+                _botao("6"),
+                _botao("x"),
+                _botao("1"),
+                _botao("2"),
+                _botao("3"),
+                _botao("-"),
+                _botao("0"),
+                _botao("."),
+                _botao("="),
+                _botao("+"),
+              ],
+            ),
           ),
-        ),
-        Expanded(
-          child: _botao(_limpar),
-        )
       ],
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,57 @@
 import 'package:flutter/material.dart';
-
 import 'calculadora.dart';
 
 void main() {
-  runApp(const App());
+  runApp(const MeuApp());
 }
 
-class App extends StatelessWidget {
-  const App({super.key});
+// Widget principal que inicia o aplicativo com o tema configurável
+class MeuApp extends StatefulWidget {
+  const MeuApp({super.key});
+
+  @override
+  _MeuAppState createState() => _MeuAppState();
+}
+
+class _MeuAppState extends State<MeuApp> {
+  // Variável para armazenar o modo do tema (claro ou escuro)
+  ThemeMode _themeMode = ThemeMode.system;
+
+  // Função para alternar entre tema claro e escuro
+  void _toggleTheme() {
+    setState(() {
+      _themeMode = _themeMode == ThemeMode.light ? ThemeMode.dark : ThemeMode.light;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Layout',
+      title: 'Calculadora',
+      // Tema claro
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomePage(title: 'Calculadora'),
+      // Tema escuro
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple, brightness: Brightness.dark),
+        useMaterial3: true,
+      ),
+      themeMode: _themeMode, // Define o modo do tema
+      home: HomePage(title: 'Calculadora', toggleTheme: _toggleTheme), // Página inicial
     );
   }
 }
 
+// Widget da página inicial
 class HomePage extends StatefulWidget {
-  const HomePage({
-    super.key,
-    required this.title,
-  });
+  const HomePage({super.key, required this.title, required this.toggleTheme});
 
   final String title;
+  final VoidCallback toggleTheme;
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -57,22 +79,18 @@ class _HomePageState extends State<HomePage> {
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
                   Expanded(
-                    child: Container(
-                        // alignment: Alignment.center,
-                        // color: Colors.red,
-                        // child: const Text('Primeira Coluna'),
-                        ),
+                    child: Container(),
                   ),
                   Expanded(
                     flex: 2,
                     child: Container(
                       alignment: Alignment.center,
                       decoration: BoxDecoration(
-                        color: Colors.grey.shade200,
+                        color: Theme.of(context).colorScheme.surface,
                         borderRadius: BorderRadius.circular(20),
                         border: Border.all(
-                          color: Colors.black,
-                          width: 1,
+                          color: Theme.of(context).colorScheme.onSurface,
+                          width: 2,
                         ),
                         boxShadow: const [
                           BoxShadow(
@@ -82,34 +100,25 @@ class _HomePageState extends State<HomePage> {
                           ),
                         ],
                       ),
-                      child: const Calculadora(),
+                      child: const Calculadora(), // Calculadora é exibida aqui
                     ),
                   ),
                   Expanded(
-                    child: Container(
-                        // alignment: Alignment.center,
-                        // color: Colors.red,
-                        // child: const Text('Terceira Coluna'),
-                        ),
+                    child: Container(),
                   ),
                 ],
               ),
             ),
             Expanded(
               flex: 2,
-              child: Container(
-                //alignment: Alignment.center,
-                //color: Colors.blue,
-                //child: const Text('Layout Inferior'),
-              ),
+              child: Container(),
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        onPressed: widget.toggleTheme, // Botão para alternar tema
+        child: const Icon(Icons.brightness_6),
       ),
     );
   }
